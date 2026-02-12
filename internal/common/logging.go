9c40e43734148c210a427f99e64b55b79e69c521
Callum Tolley <cgtrolley@gmail.com>
2026-02-12 18:41:34 +0000

    Add rest of logging

--- internal/common/logging.go
+++ internal/common/logging.go
@@ -2,9 +2,15 @@ package common
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+	"sync"
 	"time"
 )
 
@@ -31,4 +37,97 @@ func (w *logResponseWriter) WriteHeader(code int) {
 // Hijack implements http.Hijacker so WebSocket upgrades work through the logging middleware.
 func (w *logResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
 	return w.ResponseWriter.(http.Hijacker).Hijack()
+}
+
+// RotatingLogger is a simple size-based rotating logger.
+type RotatingLogger struct {
+	mu         sync.Mutex
+	path       string
+	maxSize    int64
+	maxBackups int
+	file       *os.File
+}
+
+// NewRotatingLogger creates or opens the log file and returns a RotatingLogger.
+func NewRotatingLogger(path string, maxSize int64, maxBackups int) (*RotatingLogger, error) {
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return nil, err
+	}
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &RotatingLogger{path: path, maxSize: maxSize, maxBackups: maxBackups, file: f}, nil
+}
+
+func (r *RotatingLogger) Write(p []byte) (n int, err error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	fi, err := r.file.Stat()
+	if err != nil {
+		return 0, err
+	}
+	if fi.Size()+int64(len(p)) > r.maxSize {
+		if err := r.rotate(); err != nil {
+			return 0, err
+		}
+	}
+	return r.file.Write(p)
+}
+
+func (r *RotatingLogger) rotate() error {
+	// Close current
+	if r.file != nil {
+		r.file.Close()
+	}
+	// Rename with timestamp
+	ts := time.Now().UTC().Format("20060102T150405Z")
+	newName := fmt.Sprintf("%s.%s", r.path, ts)
+	if err := os.Rename(r.path, newName); err != nil {
+		// If rename fails because file doesn't exist, ignore
+		if !os.IsNotExist(err) {
+			return err
+		}
+	}
+	// Recreate current log file
+	f, err := os.OpenFile(r.path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	r.file = f
+
+	// Enforce backups limit
+	if err := r.enforceBackups(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *RotatingLogger) enforceBackups() error {
+	dir := filepath.Dir(r.path)
+	base := filepath.Base(r.path)
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+	// collect rotated files matching base.
+	var candidates []os.DirEntry
+	for _, e := range entries {
+		name := e.Name()
+		if strings.HasPrefix(name, base+".") {
+			candidates = append(candidates, e)
+		}
+	}
+	if len(candidates) <= r.maxBackups {
+		return nil
+	}
+	// Sort by name (timestamps) ascending
+	sort.SliceStable(candidates, func(i, j int) bool { return candidates[i].Name() < candidates[j].Name() })
+	toRemove := len(candidates) - r.maxBackups
+	for i := 0; i < toRemove; i++ {
+		_ = os.Remove(filepath.Join(dir, candidates[i].Name()))
+	}
+	return nil
 }
